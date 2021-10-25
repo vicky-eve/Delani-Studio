b31abd499b1f1a24fc48930694f77f73dc953d79
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2021-10-25 20:35:56 +0300

    added the readme section

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,7 @@
-alert("string");
+
+$(document).ready(function() {
+    $(".clickable").click(function() {
+      $(".design-showing").toggle();
+      $(".design-details").toggle();
+    });
+  });
